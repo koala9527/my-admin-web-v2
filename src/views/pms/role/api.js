7d5e5e8c998c1daa0be19dce00781a0767d3a4e2
koala9527 <15549806382@qq.com>
2024-01-10 23:57:00 +0800

    系统管理正常显示

--- src/views/pms/role/api.js
+++ src/views/pms/role/api.js
@@ -10,11 +10,11 @@ import { request } from '@/utils'
 
 export default {
   create: (data) => request.post('/role', data),
-  read: (params = {}) => request.get('/role/page', { params }),
+  read: (params = {}) => request.get('/admin/role/list', { params }),
   update: (data) => request.patch(`/role/${data.id}`, data),
   delete: (id) => request.delete(`/role/${id}`),
 
-  getAllPermissionTree: () => request.get('/permission/tree'),
+  getAllPermissionTree: () => request.get('/admin/permission/tree'),
   getAllUsers: (params = {}) => request.get('/user', { params }),
   addRoleUsers: (roleId, data) => request.patch(`/role/users/add/${roleId}`, data),
   removeRoleUsers: (roleId, data) => request.patch(`/role/users/remove/${roleId}`, data),
